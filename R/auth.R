e30b220fc66bd597a03949cca67bdbb415eb7613
Daniel Russ <druss@mail.nih.gov>
2023-05-15 17:20:16 -0400

    update documentation

--- R/auth.R
+++ R/auth.R
@@ -5,18 +5,41 @@
   # api_key = NULL,
   # cred = NULL
 )
-#' Configure application for fiRstore
-#' @param app - an httr oauth application
-#'
-#' This is change in the future as gargle transitions from
-#' httr to httr2.
-#'
-#' @export
 
+
+#' Configure application for fiRstore
+#' @description
+#' Configures the httr oauth application.
+#' Gargle formerly used [httr::oauth_app]
+#' but is transitioning to [httr2::oauth_client].
+#' The fiRstore package is following the other packages such as
+#' bigrquery by having an auth_configure function that takes
+#' a path and uses an [httr2::oauth_client].
+#'
+#'
+#' @param app
+#' an httr oauth application
+#'
+#' @param path
+#' The path to the JSON file containing
+#' the client Id/password associated with the GCP
+#' project.
+#'
+
+#' @note
+#' This  change in the future as gargle continues the transition
+#' from httr to httr2.
+#'
+#' @seealso [bigrquery::bq_auth_configure], [googledrive::drive_auth_config],
+#' [googledrive::drive_auth_configure]
+#' @export
 fiRstore_auth_config<-function(app){
   .auth$set_app(app)
 }
 
+#' @rdname fiRstore_auth_config
+#' @export
+#'
 fiRstore_auth_configure <- function(path){
   .auth$set_app( gargle::gargle_oauth_client_from_json(path) )
 }
